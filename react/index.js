1312205f07589572f7376b70d4d228f8236b5755
archist <webdev.archist@google.com>
2022-02-13 18:55:39 +0100

    rectifié petit bug sur react/index.js

--- react/index.js
+++ react/index.js
@@ -6,5 +6,5 @@ import App_ from './App_.js'
 import App_0 from './App_0.js'
 import json from "./quiz.json"
 
-// render(<App type="basic" json={json} />, document.querySelector('#root'))
-render(<App_ />, document.querySelector('#root'))
+render(<App type="basic" json={json} />, document.querySelector('#root'))
+// render(<App_ />, document.querySelector('#root'))
